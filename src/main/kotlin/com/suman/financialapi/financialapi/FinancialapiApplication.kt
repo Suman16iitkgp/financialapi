308a5f20adcfe67c7b93643c3b50b4f33d6b8652
Suman <sk1244957@gmail.com>
2025-09-28 14:50:38 +0200

    Initial Version

---
+++ src/main/kotlin/com/suman/financialapi/financialapi/FinancialapiApplication.kt
@@ -0,0 +1,11 @@
+package com.suman.financialapi.financialapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinancialapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinancialapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/suman/financialapi/financialapi/FinancialapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.suman.financialapi.financialapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinancialapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
